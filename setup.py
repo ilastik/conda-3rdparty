aec7b2e180f7ead309712ff71c14278f1233b953
k-dominik <k-dominik@users.noreply.github.com>
2025-12-23 04:00:59 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="conda-3rdparty",
-    version="0.0.5",
+    version="0.0.6",
     author="ilastik-team",
     license="MIT",
     license_files=("LICENSE",),
